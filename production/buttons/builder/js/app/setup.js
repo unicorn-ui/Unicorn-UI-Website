67a4455e4e39a2da3a8a88d53e29d682508563b8
Rob Levin <pair+rob@mavenlink.com>
2014-11-16 21:10:55 -0800

    WIP - porting builder to use buttons 2.0 buttons

--- production/buttons/builder/js/app/setup.js
+++ production/buttons/builder/js/app/setup.js
@@ -1,82 +1,247 @@
 (function(){
-    'use strict';
+  'use strict';
 
-    /*globals Backbone, _, $ */
+  /*globals Backbone, _, $ */
 
 
-    ////////////////////////////////////////////////
-    // BACKBONE CUSTOM SETTINGS ////////////////////
-    ////////////////////////////////////////////////
+  ////////////////////////////////////////////////
+  // BACKBONE CUSTOM SETTINGS ////////////////////
+  ////////////////////////////////////////////////
 
-    Backbone.emulateHTTP = true;
+  Backbone.emulateHTTP = true;
 
-    Backbone.sync = function(method, model, options) {
-        options = options || {};
+  Backbone.sync = function(method, model, options) {
+    options = options || {};
 
-        var params = {
-            type: 'POST',
-            dataType: 'jsonp',
-            data: model.toJSON(method, model),
-            url: model.url,
-        };
-
-        var data = _.extend(params, options);
-
-        return $.ajax(data);
+    var params = {
+      type: 'POST',
+      dataType: 'jsonp',
+      data: model.toJSON(method, model),
+      url: model.url,
     };
 
+    var data = _.extend(params, options);
+
+    return $.ajax(data);
+  };
 
 
-    ////////////////////////////////////////////////
-    // APP NAMESPACE ///////////////////////////////
-    ////////////////////////////////////////////////
-    window.Unicorn = {
-        Models: {},
-        Views: {},
-        Options: {
-            serverUrl: 'http://options-compiler.herokuapp.com',
-            //serverUrl: 'http://localhost:5000',
-            name: 'buttons',
-            'btn-namespace': '.button',//MUST START OUT CONSISTENT WITH CLASS USED IN INITIAL MARKUP!
-            'btn-name': 'button',
-            'btn-glow-namespace': '.glow',
-            'btn-glow-name': 'glow',
-            'btn-font-color': '#666',
-            'btn-font-size': '14px',
-            'btn-font-weight': 300,
-            'btn-font-family': ['Helvetica Neue Light', 'Helvetica Neue', 'Helvetica', 'Arial', 'Lucida Grande', 'sans-serif'],
-            'btn-actions': [
-                {
-                    name: 'primary',
-                    background: '#00A1CB',
-                    color: '#FFFFFF'
-                },
-                {
-                    name: 'action',
-                    background: '#7db500',
-                    color: '#FFFFFF'
-                },
-                {
-                    name: 'highlight',
-                    background: '#F18D05',
-                    color: '#FFFFFF'
-                },
-                {
-                    name: 'caution',
-                    background: '#E54028',
-                    color: '#FFFFFF'
-                },
-                {
-                    name: 'royal',
-                    background: '#87318C',
-                    color: '#FFFFFF'
-                }
-            ]
-        }
-    };
+
+  ////////////////////////////////////////////////
+  // APP NAMESPACE ///////////////////////////////
+  ////////////////////////////////////////////////
+  window.Unicorn = {
+    Models: {},
+    Views: {},
+    Options: {
+        serverUrl: 'http://localhost:5000',
+//            serverUrl: 'http://options-compiler.herokuapp.com',
+          
+        'btn-namespace': '.robs-buttons',
+        'btn-colors': [
+          {
+            name: 'primary',
+            background: '#00A1CB',
+            color: '#FFFFFF'
+          },
+          {
+            name: 'action',
+            background: '#7db500',
+            color: '#FFFFFF'
+          },
+          {
+            name: 'highlight',
+            background: '#F18D05',
+            color: '#FFFFFF'
+          },
+          {
+            name: 'caution',
+            background: '#E54028',
+            color: '#FFFFFF'
+          },
+          {
+            name: 'silly',
+            background: '#87318C',
+            color: '#FFFFFF'
+          }
+        ],
+        'btn-shapes': [
+          {
+            name: 'pill',
+            radius: '200px'
+          },
+          {
+            name: 'rounded',
+            radius: '3px'
+          }
+        ],
+        'btn-sizes': [
+          {
+            name: 'giant',
+            multiplier: '2'
+          },
+          {
+            name: 'jumbo',
+            multiplier: '1.5'
+          },
+          {
+            name: 'large',
+            multiplier: '1.25'
+          },
+          {
+            name: 'normal',
+            multiplier: '1'
+          },
+          {
+            name: 'small',
+            multiplier: '.75'
+          },
+          {
+            name: 'tiny',
+            multiplier: '.5'
+          }
+        ],
+        'types': ['border', 'shapes'],
+        'btn-height': '40px',
+        'btn-bgcolor': '#EEE',
+        'btn-font-color': '#666',
+        'btn-font-size': '14px',
+        'btn-font-weight': '300',
+        'btn-font-family': ['Arial', 'sans-serif']           
+        
+          
+//            name: 'buttons',
+//            'btn-namespace': '.button',//MUST START OUT CONSISTENT WITH CLASS USED IN INITIAL MARKUP!
+//            'btn-name': 'button',
+//            'btn-glow-namespace': '.glow',
+//            'btn-glow-name': 'glow',
+//            'btn-font-color': '#666',
+//            'btn-font-size': '14px',
+//            'btn-font-weight': 300,
+//            'btn-font-family': ['Helvetica Neue Light', 'Helvetica Neue', 'Helvetica', 'Arial', 'Lucida Grande', 'sans-serif'],
+//            'btn-actions': [
+//                {
+//                    name: 'primary',
+//                    background: '#00A1CB',
+//                    color: '#FFFFFF'
+//                },
+//                {
+//                    name: 'action',
+//                    background: '#7db500',
+//                    color: '#FFFFFF'
+//                },
+//                {
+//                    name: 'highlight',
+//                    background: '#F18D05',
+//                    color: '#FFFFFF'
+//                },
+//                {
+//                    name: 'caution',
+//                    background: '#E54028',
+//                    color: '#FFFFFF'
+//                },
+//                {
+//                    name: 'royal',
+//                    background: '#87318C',
+//                    color: '#FFFFFF'
+//                }
+//            ]
+    }
+  };
 
 })();
 
 
 
+/**
+* Buttons Module
+*/
+
+/*
+var ButtonsModule = ButtonsModule || {};
+
+ButtonsModule.buttonDefaultOptions = {
+  'btn-namespace': '.robs-buttons',
+  'btn-colors': [
+    {
+        name: 'primary',
+        background: '#00A1CB',
+        color: '#FFFFFF'
+    },
+    {
+        name: 'action',
+        background: '#7db500',
+        color: '#FFFFFF'
+    },
+    {
+        name: 'highlight',
+        background: '#F18D05',
+        color: '#FFFFFF'
+    },
+    {
+        name: 'caution',
+        background: '#E54028',
+        color: '#FFFFFF'
+    },
+    {
+        name: 'silly',
+        background: '#87318C',
+        color: '#FFFFFF'
+    }
+  ],
+  'btn-shapes': [
+    {
+      name: 'pill',
+      radius: '200px'
+    },
+    {
+      name: 'rounded',
+      radius: '3px'
+    }
+  ],
+  'btn-sizes': [
+    {
+      name: 'giant',
+      multiplier: '2'
+    },
+    {
+      name: 'jumbo',
+      multiplier: '1.5'
+    },
+    {
+      name: 'large',
+      multiplier: '1.25'
+    },
+    {
+      name: 'normal',
+      multiplier: '1'
+    },
+    {
+      name: 'small',
+      multiplier: '.75'
+    },
+    {
+      name: 'tiny',
+      multiplier: '.5'
+    }
+  ],
+  'types': ['border', 'shapes'],
+  'btn-height': '40px',
+  'btn-bgcolor': '#EEE',
+  'btn-font-color': '#666',
+  'btn-font-size': '14px',
+  'btn-font-weight': '300',
+  'btn-font-family': ['Arial', 'sans-serif']
+};
+
+*/
+
+//ButtonsModule.Model = JsonpModel.extend({
+//  module: 'buttons',
+//  defaults: ButtonsModule.buttonDefaultOptions,
+//  url: function() {
+//    // return 'http://options-compiler.herokuapp.com/build/buttons';
+//    return 'http://localhost:5000/build/'+this.module;
+//  }
+//});
 
--- public/buttons/builder/js/buttons.js
+++ public/buttons/builder/js/buttons.js
@@ -1,110 +1,115 @@
-/*
- *  Project: Buttons
- *  Description: A highly customizable CSS button library built with Sass and Compass
- *  Author: Alex Wolfe
- *  License: Apache License v2.0
- */
+/*! @license
+*  Project: Buttons
+*  Description: A highly customizable CSS button library built with Sass and Compass
+*  Author: Alex Wolfe and Rob Levin
+*  License: Apache License v2.0
+*/
+
 
 // the semi-colon before function invocation is a safety net against concatenated
 // scripts and/or other plugins which may not be closed properly.
 ;(function ( $, window, document, undefined ) {
-    'use strict';
+  'use strict';
 
-    // undefined is used here as the undefined global variable in ECMAScript 3 is
-    // mutable (ie. it can be changed by someone else). undefined isn't really being
-    // passed in so we can ensure the value of it is truly undefined. In ES5, undefined
-    // can no longer be modified.
+  // undefined is used here as the undefined global variable in ECMAScript 3 is
+  // mutable (ie. it can be changed by someone else). undefined isn't really being
+  // passed in so we can ensure the value of it is truly undefined. In ES5, undefined
+  // can no longer be modified.
 
-    // window and document are passed through as local variable rather than global
-    // as this (slightly) quickens the resolution process and can be more efficiently
-    // minified (especially when both are regularly referenced in your plugin).
+  // window and document are passed through as local variable rather than global
+  // as this (slightly) quickens the resolution process and can be more efficiently
+  // minified (especially when both are regularly referenced in your plugin).
 
-    // Create the defaults once
-    var menuButton = 'menuButton';
-    var defaults = {
-        propertyName: 'value'
-    };
+  // Create the defaults once
+  var pluginName = "menuButton";
+  var menuClass = ".button-dropdown";
+  var defaults = {
+    propertyName: "value"
+  };
 
-    // The actual plugin constructor
-    function Plugin( element, options ) {
+  // The actual plugin constructor
+  function Plugin( element, options ) {
 
-        //SET OPTIONS
-        this.options = $.extend( {}, defaults, options );
-        this._defaults = defaults;
-        this._name = menuButton;
+    //SET OPTIONS
+    this.options = $.extend( {}, defaults, options );
+    this._defaults = defaults;
+    this._name = pluginName;
 
-        //REGISTER ELEMENT
-        this.$element = $(element);
+    //REGISTER ELEMENT
+    this.$element = $(element);
 
-        //INITIALIZE
-        this.init();
+    //INITIALIZE
+    this.init();
+  }
+
+  Plugin.prototype = {
+    constructor: Plugin,
+
+    init: function() {
+      // WE DON'T STOP PROPGATION SO CLICKS WILL AUTOMATICALLY
+      // TOGGLE AND REMOVE THE DROPDOWN
+      this.toggle();
+    },
+
+    toggle: function(el, options) {
+      if(this.$element.data('dropdown') === 'show') {
+        this.hideMenu();
+      }
+      else {
+        this.showMenu();
+      }
+    },
+
+    showMenu: function() {
+      this.$element.data('dropdown', 'show');
+      this.$element.find('ul').show();
+      this.$element.find('.button:first').addClass('is-active');
+    },
+
+    hideMenu: function() {
+      this.$element.data('dropdown', 'hide');
+      this.$element.find('ul').hide();
+      this.$element.find('.button:first').removeClass('is-active');
     }
+  };
 
-    Plugin.prototype = {
-        constructor: Plugin,
+  // A really lightweight plugin wrapper around the constructor,
+  // preventing against multiple instantiations
+  $.fn[pluginName] = function ( options ) {
+    return this.each(function () {
 
-        init: function() {
-            // WE DON'T STOP PROPGATION SO CLICKS WILL AUTOMATICALLY
-            // TOGGLE AND REMOVE THE DROPDOWN & OVERLAY
-            this.toggle();
-        },
+      // TOGGLE BUTTON IF IT EXISTS
+      if ($.data(this, "plugin_" + pluginName)) {
+        $.data(this, "plugin_" + pluginName).toggle();
+      }
+      // OTHERWISE CREATE A NEW INSTANCE
+      else {
+        $.data(this, "plugin_" + pluginName, new Plugin( this, options ));
+      }
+    });
+  };
 
-        //function(el, options) are avaialble in toggle method
-        toggle: function() {
-            if(this.$element.data('dropdown') === 'show') {
-                this.hideMenu();
-            }
-            else {
-                this.showMenu();
-            }
-        },
-
-        showMenu: function() {
-            this.$element.data('dropdown', 'show');
-            this.$element.find('ul').show();
-
-            if(this.$overlay) {
-                this.$overlay.show();
-            }
-            else {
-                this.$overlay = $('<div style="position: fixed; top: 0px;left: 0px; right: 0px; bottom: 0px; z-index: 999;"></div>');
-                this.$element.append(this.$overlay);
-            }
-        },
-
-        hideMenu: function() {
-            this.$element.data('dropdown', 'hide');
-            this.$element.find('ul').hide();
-            this.$overlay.hide();
+  //CLOSE OPEN DROPDOWN MENUS IF CLICKED SOMEWHERE ELSE
+  $(document).on('click', function(e) {
+    $.each($('[data-buttons=dropdown]'), function(i, value) {
+      if ($(e.target.offsetParent)[0] != $(this)[0]) {
+        if ($.data(this, "plugin_" + pluginName)) {
+          $.data(this, "plugin_" + pluginName).hideMenu();
+          $(this).find('ul').hide();
         }
-    };
-
-    // A really lightweight plugin wrapper around the constructor,
-    // preventing against multiple instantiations
-    $.fn[menuButton] = function ( options ) {
-        return this.each(function () {
-
-            // TOGGLE BUTTON IF IT EXISTS
-            if ($.data(this, 'plugin_' + menuButton)) {
-                $.data(this, 'plugin_' + menuButton).toggle();
-            }
-            // OTHERWISE CREATE A NEW INSTANCE
-            else {
-                $.data(this, 'plugin_' + menuButton, new Plugin( this, options ));
-            }
-        });
-    };
-
-
-    //DELEGATE CLICK EVENT FOR DROPDOWN MENUS
-    $(document).on('click', '[data-buttons=dropdown]', function(e) {
-        var $dropdown = $(e.currentTarget);
-        $dropdown.menuButton();
+      }
     });
+  });
 
-    //IGNORE CLICK EVENTS FROM DISPLAY BUTTON IN DROPDOWN
-    $(document).on('click', '[data-buttons=dropdown] > a', function(e) {
-        e.preventDefault();
-    });
+  //DELEGATE CLICK EVENT FOR DROPDOWN MENUS
+  $(document).on('click', '[data-buttons=dropdown]', function(e) {
+    var $dropdown = $(e.currentTarget);
+    $dropdown.menuButton();
+  });
+
+  //IGNORE CLICK EVENTS FROM DISPLAY BUTTON IN DROPDOWN
+  $(document).on('click', '[data-buttons=dropdown] > a', function(e) {
+    e.preventDefault();
+  });
 
 })( jQuery, window, document);
