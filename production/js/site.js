91c709cf6a74a210eb7be2fa0545f9855f95eb0f
Alex Wolfe <thewolfe@gmail.com>
2015-01-02 16:41:29 -0800

    Pretty Dates

--- production/js/site.js
+++ production/js/site.js
@@ -47,7 +47,21 @@
 	var Website = {
 
 	  init: function() {
+	    this.formatDates();
+	  },
 
+	  formatDates: function() {
+	    var dates = $('.article-date');
+
+	    if(dates.length) {
+	      _.each(dates, function(dateElement) {
+	        var $dateElement = $(dateElement);
+	        var currentTime = $dateElement.text();
+	        var prettyDate = moment(currentTime).format("MMM Do YYYY");
+
+	        $dateElement.text(prettyDate);
+	      });
+	    }
 	  }
 
 	};
--- resources/js/site.js
+++ resources/js/site.js
@@ -1,7 +1,21 @@
 var Website = {
 
   init: function() {
+    this.formatDates();
+  },
 
+  formatDates: function() {
+    var dates = $('.article-date');
+
+    if(dates.length) {
+      _.each(dates, function(dateElement) {
+        var $dateElement = $(dateElement);
+        var currentTime = $dateElement.text();
+        var prettyDate = moment(currentTime).format("MMM Do YYYY");
+
+        $dateElement.text(prettyDate);
+      });
+    }
   }
 
 };
